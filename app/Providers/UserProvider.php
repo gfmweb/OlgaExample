6931f748e0d2d482a2da72c4970828228692f0b9
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-05-29 11:34:54 +0500

    Инициализация сервис провайдера и объявление в нем подключения к репозиторию

--- app/Providers/UserProvider.php
+++ app/Providers/UserProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\DataAccess\Interfaces\UserRepository;
+use App\DataAccess\Repositories\UserRepository as Repository;
 use Illuminate\Support\ServiceProvider;
 
 class UserProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class UserProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind(UserRepository::class,Repository::class);
     }
 }
