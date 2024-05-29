fef71b66fc9d8800686ca5b375ed8b9362497de8
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-05-29 10:43:47 +0500

    UserDTO (begin)

--- app/DataAccess/DTO/UserDTO.php
+++ app/DataAccess/DTO/UserDTO.php
@@ -4,5 +4,19 @@ namespace App\DataAccess\DTO;
 
 class UserDTO
 {
+    public int $id;
+    public string $name;
+    public string $email;
 
+    private function __construct(array $data)
+    {
+        $this->id = $data['id'];
+        $this->name = $data['name'];
+        $this->email = $data['emial'];
+    }
+
+    public static function makeFromModel(User $user)
+    {
+        return new self($user->toArray());
+    }
 }
