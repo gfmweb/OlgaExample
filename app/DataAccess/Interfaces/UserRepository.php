53fb5796dff8797ae94716bbd2241da39a0ba483
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-05-29 10:52:30 +0500

    UserInterface

--- app/DataAccess/Interfaces/UserRepository.php
+++ app/DataAccess/Interfaces/UserRepository.php
@@ -2,7 +2,18 @@
 
 namespace App\DataAccess\Interfaces;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
 interface UserRepository
 {
+    public function showAll():Collection;
+    public function getById(int $id):User;
+    public function getByEmail(int $id):User;
+
+    public function update(array $data):User;
+    public function create(array $data):User;
+
+    public function destroy(int $id):bool;
 
 }
