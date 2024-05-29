8c0e9f2bfdbe625c745f7d3c9b08c8b105171a25
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-05-29 11:50:23 +0500

    Тестовый контроллер

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\DataAccess\Interfaces\UserRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index(UserRepository $userRepository):JsonResponse
+    {
+        return response()->json($userRepository->showAll());
+    }
+}
