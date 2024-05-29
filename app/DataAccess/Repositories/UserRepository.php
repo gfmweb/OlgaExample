851a3dab00079c2be96cf4d95ca8e4334d7ef96e
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-05-29 10:58:39 +0500

    Задействуем интерфейс

--- app/DataAccess/Repositories/UserRepository.php
+++ app/DataAccess/Repositories/UserRepository.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\DataAccess\Repositories;
+use App\DataAccess\Interfaces\UserRepository as interfaceRepository;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
-class UserRepository
+class UserRepository implements interfaceRepository
 {
+
 
 }
