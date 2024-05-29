7a50bcc01b0f061594ae0048498cf6025a3c9d62
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-05-29 10:54:21 +0500

    bugFix

--- app/DataAccess/Interfaces/UserRepository.php
+++ app/DataAccess/Interfaces/UserRepository.php
@@ -11,7 +11,7 @@ interface UserRepository
     public function getById(int $id):User;
     public function getByEmail(string $email):User;
 
-    public function update(array $data):User;
+    public function update(int $id, array $data):User;
     public function create(array $data):User;
 
     public function destroy(int $id):bool;
