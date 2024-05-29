44c8a727760766c111c0d997d6708e5372f821b2
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-05-29 11:00:30 +0500

    пример возврата всех пользователей

--- app/DataAccess/Repositories/UserRepository.php
+++ app/DataAccess/Repositories/UserRepository.php
@@ -9,4 +9,33 @@ class UserRepository implements interfaceRepository
 {
 
 
+    public function showAll(): Collection
+    {
+        return User::all();
+    }
+
+    public function getById(int $id): User
+    {
+        // TODO: Implement getById() method.
+    }
+
+    public function getByEmail(string $email): User
+    {
+        // TODO: Implement getByEmail() method.
+    }
+
+    public function update(int $id, array $data): User
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function create(array $data): User
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function destroy(int $id): bool
+    {
+        // TODO: Implement destroy() method.
+    }
 }
